6a81a0ebd46f659d616f94df43148680f29c96d3
Marc Nyffenegger <marcnyffenegger@gmail.com>
2014-09-25 16:33:14 +0200

    LED Components added

--- sumo_frdm/Sources/main.c
+++ sumo_frdm/Sources/main.c
@@ -30,8 +30,12 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
-#include "Bit1.h"
 #include "BitIoLdd1.h"
+#include "LED2.h"
+#include "BitIoLdd2.h"
+#include "LED3.h"
+#include "BitIoLdd3.h"
+#include "LED1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
