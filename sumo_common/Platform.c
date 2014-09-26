f022a2c2e59d7831fb79eeaa39c1c25502ce3c0b
Marc Nyffenegger <marcnyffenegger@gmail.com>
2014-09-26 13:57:52 +0200

    added platform

---
+++ sumo_common/Platform.c
@@ -0,0 +1,17 @@
+
+#include "Platform.h"
+#if PL_HAS_LED
+  #include "LED.h"
+#endif
+
+void PL_Init(void) {
+#if PL_HAS_LED
+  LED_Init();
+#endif  
+}
+
+void PL_Deinit(void) {
+#if PL_HAS_LED
+  LED_Deinit();
+#endif
+}
